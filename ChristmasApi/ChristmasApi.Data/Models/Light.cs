2313c439b6ffe1dcd8fadff2e04fdbb565201daf
KKDinev20 <KKDinev20@codingburgas.bg>
2024-12-18 10:31:43 +0200

    Refactor Light Model

--- ChristmasApi/ChristmasApi.Data/Models/Light.cs
+++ ChristmasApi/ChristmasApi.Data/Models/Light.cs
@@ -1,32 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace ChristmasApi.Data.Models;
+﻿namespace ChristmasApi.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
 
 public class Light
 {
     [Required]
-    public int Id { get; set; } 
-    
-    [Range(0, int.MaxValue, ErrorMessage = "X coordinate must be a non-negative integer.")]
-    public int X { get; set; } // Horizontal coordinate
-    
-    [Range(0, int.MaxValue, ErrorMessage = "Y coordinate must be a non-negative integer.")]
-    public int Y { get; set; } // Vertical coordinate
-    
-    [Range(3, 6, ErrorMessage = "Radius must be between 3 and 6.")]
-    public double Radius { get; set; } // Light radius
-    
+    public int Id { get; set; }
+
+    [Range(0, double.MaxValue)]
+    public double X { get; set; }
+
+    [Range(0, double.MaxValue)]
+    public double Y { get; set; }
+
+    [Range(3, 6)]
+    public double Radius { get; set; }
+
     [Required]
-    [RegularExpression("blue-lt|blue-dk|red|gold-lt|gold-dk", ErrorMessage = "Invalid color. Valid values are: blue-lt, blue-dk, red, gold-lt, gold-dk.")]
-    public string Color { get; set; } // Light color
-    
+    [RegularExpression("blue-lt|blue-dk|red|gold-lt|gold-dk")]
+    public required string Color { get; set; }
+
     [Required]
-    [RegularExpression("g1|g2|g3", ErrorMessage = "Invalid effect. Valid values are: g1, g2, g3.")]
-    public string Effects { get; set; } // Light effects
-    
-    [StringLength(500, ErrorMessage = "Description must be less than 500 characters.")]
-    public string Desc { get; set; } // Description (provided from HTTP POST request body)
-    
+    [RegularExpression("g1|g2|g3")]
+    public required string Effects { get; set; }
+
+    [StringLength(500)]
+    public required string Description { get; set; }
+
     [Required]
-    [StringLength(500, ErrorMessage = "Christmas-Token must be less than 500 characters.")]
-    public string Ct { get; set; } // Christmas-Token (provided from HTTP POST request header)
+    public required string ChristmasToken { get; set; }
 }
--- ChristmasApi/ChristmasApi.Main/Program.cs
+++ ChristmasApi/ChristmasApi.Main/Program.cs
@@ -6,16 +6,13 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddAuthorization();
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -25,7 +22,7 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-        
+
         app.Run();
     }
 }
