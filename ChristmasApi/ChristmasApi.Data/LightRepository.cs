35e5daaa89fbffccf0857860989b1ab8a0eb7023
vvbolashikova20 <vvbolashikova20@codingburgas.bg>
2024-12-19 07:51:59 +0200

    Add light repository

---
+++ ChristmasApi/ChristmasApi.Data/LightRepository.cs
@@ -0,0 +1,30 @@
+﻿using ChristmasApi.Data.Models;
+
+namespace ChristmasApi.Data;
+
+public class LightRepository
+{
+    private readonly ChristmasApiDbContext dbContext;
+
+    public LightRepository(ChristmasApiDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async void CreateLight(Light light)
+    {
+        await this.dbContext.Lights.AddAsync(light);
+        await this.dbContext.SaveChangesAsync();
+    }
+
+    public IEnumerable<Light>GetLights()
+    {
+        return this.dbContext.Lights.ToList();
+    }
+
+    public void DeleteLight(Light light)
+    {
+        this.dbContext.Lights.Remove(light);
+        this.dbContext.SaveChanges();
+    }
+}
